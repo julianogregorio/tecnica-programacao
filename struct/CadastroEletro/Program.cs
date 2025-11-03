04fa6f2922d7fe561437fa28b69b710e75b35672
Juliano <juliano.gregorio@alunos.ifsuldeminas.edu.br>
2025-11-03 11:06:18 -0300

    add cadastroEletro parcial

---
+++ struct/CadastroEletro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CadastroEletro{
+
+    static void addEletro(List<Eletro> lista){
+
+        Eletro novoEletro = new Eletro();
+        Console.Write("Entre com o nome: ");
+        novoEletro.nome = Console.ReadLine();  
+        
+        Console.Write("Entre com a potência: ");
+        novoEletro.potencia = double.Parse(Console.ReadLine());  
+        
+        Console.Write("Entre com o tempo médio de uso diário: ");
+        novoEletro.tempoMedioUsoDiario = double.Parse(Console.ReadLine());
+
+        listaEletros.add(novoEletro);  
+
+    }
+    
+    
+    static void Main(){
+        List <Eletro> listaEletros = new list <Eletro>();
+    }
+
+
+}
